530a61ebd701793ac82b5cd637057a90cb050a83
Saikat Chakraborty <saikatgpae@gmail.com>
2022-08-05 12:42:27 +0530

    Remove linters error.

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-document.querySelector('.header').addEventListener('click',()=>{
+document.querySelector('.header').addEventListener('click', () => {
+  // Creating the X sumbol by changing the .bar div position.
+  document.querySelector('#bar1').classList.toggle('bar1-clicked');
+  document.querySelector('#bar2').classList.toggle('bar2-clicked');
+  document.querySelector('#bar3').classList.toggle('bar3-clicked');
 
-    //Creating the X sumbol by changing the .bar div position.
-    document.querySelector('#bar1').classList.toggle('bar1-clicked');
-    document.querySelector('#bar2').classList.toggle('bar2-clicked');
-    document.querySelector('#bar3').classList.toggle('bar3-clicked');
-
-    //poping up the mobile menu
-    document.querySelector('.mobile-menu').classList.toggle('mobile-menu-trans')  
+  // poping up the mobile menu
+  document.querySelector('.mobile-menu').classList.toggle('mobile-menu-trans');
 });
 
-
- //closing the popup menu onclicking the popup menu items
+// closing the popup menu onclicking the popup menu items
 
 const menu = Array.from(document.querySelectorAll('.menu a'));
-menu.forEach((link)=>{link.addEventListener('click',()=>{
+menu.forEach((link) => {
+  link.addEventListener('click', () => {
     document.querySelector('.mobile-menu').classList.remove('mobile-menu-trans');
     document.querySelector('#bar1').classList.remove('bar1-clicked');
     document.querySelector('#bar2').classList.remove('bar2-clicked');
     document.querySelector('#bar3').classList.remove('bar3-clicked');
+  });
+});
 
-})});
-
-
-//activating the dropdown menu
+// activating the dropdown menu
 const dropDown = document.querySelector('.dropdown-arrow');
-dropDown.addEventListener('click',()=>{
-    //dispalying the hidden speakers
-    document.querySelector('.speaker-6').classList.toggle('dropdown-dispaly');
-    document.querySelector('.speaker-5').classList.toggle('dropdown-dispaly');
-    document.querySelector('.speaker-4').classList.toggle('dropdown-dispaly');
-    
-    //turning the arrow sighn to 180 degree
-    document.querySelector('.dropdown-arrow').classList.toggle('dropdown-arrow-reverse');
+dropDown.addEventListener('click', () => {
+  // dispalying the hidden speakers
+  document.querySelector('.speaker-6').classList.toggle('dropdown-dispaly');
+  document.querySelector('.speaker-5').classList.toggle('dropdown-dispaly');
+  document.querySelector('.speaker-4').classList.toggle('dropdown-dispaly');
+
+  // turning the arrow sighn to 180 degree
+  document.querySelector('.dropdown-arrow').classList.toggle('dropdown-arrow-reverse');
 });
